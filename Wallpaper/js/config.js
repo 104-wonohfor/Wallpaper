34e479e167c92f28c5de975787e92dc00919a638
104 <104601534+104-wonohfor@users.noreply.github.com>
2022-08-23 20:31:12 +0700

    Add files via upload

---
+++ Wallpaper/js/config.js
@@ -0,0 +1,16 @@
+﻿
+
+var config={
+
+    'language_type':3,
+    'font_color':'#ef5b0c',
+    'pointer_color':'#dff6FF',
+
+    'sound':0,
+    'sound_name':'bg.mp3',
+
+    'background_style':0,
+    'background_picture':'bg.jpg',
+    'background_video':'3.mp4',
+    'background_color':'#000000',
+}
